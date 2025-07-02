6f9b72ed7ba4d3c6f181e3c8ae5790233f279255
67D48D5 <kernel@yonsei.ac.kr>
2025-07-02 13:08:10 +0900

    Draft scripts

--- scripts/utils/install.sh
+++ scripts/utils/install.sh
@@ -1,23 +1,36 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_PATH="$(realpath "${SCRIPT_DIR}/../..")"
+
 # Prevent device sleep
 termux-wake-lock
 
 # Termux storage setup
 termux-setup-storage
 
-# Install necessary packages
+# Update package lists and upgrade installed packages
 pkg update && pkg upgrade -y
-pkg install -y termux-root termux-services openssh git wget curl jq openjdk-21 zsh mariadb tmux
+
+# Install necessary packages
+pkg install -y termux-root termux-services tsu
+pkg install -y zsh openssh mosh git wget curl jq openjdk-21 tmux jq mariadb tmux tree fzf zip unzip neovim vim
+
+# Enable Termux services
+bash -c "sv-enable mysqld && sv-enable sshd"
 
 # Move existing .termux directory and create a symlink
 mv ~/.termux ~/.termux.bak
-ln -s ~/everest/.termux ~/.termux
+ln -sf $(realpath ${ROOT_PATH}/.termux) ~/.termux
 
-# @TODO: Complete the installation process
-sudo cat
+# Allow script execution
+chmod -R +x ${ROOT_PATH}/scripts
+chmod +x ~/.termux/boot/*.sh ~/.termux/optimizer.sh
 
+# Create or append to the Termux startup script for Magisk
+STARTUP_SCRIPT="/data/adb/services.d/start-termux.sh"
+sudo cat <<'EOF' | sudo tee "${STARTUP_SCRIPT}" >/dev/null
 #!/system/bin/sh
 
 am start -n com.termux/.HomeActivity
@@ -33,3 +46,7 @@ while true; do
     fi
     sleep 10
 done
+EOF
+
+# Ensure the startup script is executable
+sudo chmod +x "${STARTUP_SCRIPT}"
