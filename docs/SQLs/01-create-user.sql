f8169fd88179f1e85c81fcf0e95350f7bb4e2574
F911E41 <kernel@yonsei.ac.kr>
2026-02-03 12:51:58 +0900

    Remove typo space

--- docs/SQLs/01-create-user.sql
+++ docs/SQLs/01-create-user.sql
@@ -1,8 +1,8 @@
 -- Create the local user with the specified password
-CREATE USER '46770d6' @'localhost' IDENTIFIED BY '7f04916';
+CREATE USER '46770d6'@'localhost' IDENTIFIED BY '7f04916';
 
 -- Grant all privileges to the user on all databases
-GRANT ALL PRIVILEGES ON *.* TO '46770d6' @'localhost';
+GRANT ALL PRIVILEGES ON *.* TO '46770d6'@'localhost';
 
 -- Apply the changes
 FLUSH PRIVILEGES;
